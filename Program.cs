c87025e6ff402d94ef924215e1c0e2b8441b9278
Aigerim <aigerim.10803@gmail.com>
2025-12-12 21:31:37 +0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -11,10 +11,20 @@
             Console.WriteLine("Hello, C# Academy! One more modification!");
             Console.WriteLine("Hello, C# Academy! One more modification!");
             MyMethod();
+            MyMethod2();
+            MyMethod3();
         }
         public static void MyMethod()
         {
             Console.WriteLine("This is my Method");
+        }
+        public static void MyMethod2()
+        {
+            Console.WriteLine("This is my Method 2");
+        }
+        public static void MyMethod3()
+        {
+            Console.WriteLine("This is my Method 3");
         }
     }
 }
